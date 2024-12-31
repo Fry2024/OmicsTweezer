655b2bf8b617df54da3077ebf157b34207a4f989
Fry2024 <yangxin@ohsu.edu>
2024-12-30 17:20:27 -0800

    Add files via upload

---
+++ OmicsTweezer/train_predict.py
@@ -0,0 +1,22 @@
+from OmicsTweezer import simulation
+from sklearn.preprocessing import MinMaxScaler
+from OmicsTweezer import deconvolution
+
+def train_predict(train_reference, test_bulk, num, scale, ot_weight):
+    
+    ## Sampling Training Samples
+    train_bulk = simulation.generate_simulated_data(train_reference,samplenum=num,sparse=False)
+    if scale:
+        ## Min-Max Scaling
+        scaler = MinMaxScaler()
+        train_bulk.X = scaler.fit_transform(train_bulk.X)
+        test_bulk.X = scaler.fit_transform(test_bulk.X)
+
+
+    test_bulk.obs = train_bulk.obs.iloc[0:test_bulk.shape[0],:]
+    
+    ## train and predict
+    predict_output, ground_truth = deconvolution.mian(train_bulk, test_bulk,  ot_weight,sep='\t',
+                               batch_size=128, epochs=30)
+
+    return (predict_output[0]+predict_output[1]+predict_output[2])/3
